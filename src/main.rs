4601909d09082e55826530f3242545d4d9d18f7e
Ernesto2018 <ernestondieki12@gmail.com>
2025-10-28 11:43:31 +0300

    Enums: Concise Control Flow with `if let` and `let else`

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+#[derive(Debug)] // so we can inspect the state
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+        }
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn describe_state_quarter(coin: &Coin) -> Option<String> {
+    let state = if let Coin::Quarter(state) = coin {
+        state
+    } else {
+        // return from the function entirely
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
+    }
+}
+
+fn main() {
+    let coin = Coin::Quarter(UsState::Alaska);
+
+    value_in_cents(&coin);
+
+    if let Some(desc) = describe_state_quarter(&coin) {
+        println!("{}", desc);
+    }
+}
