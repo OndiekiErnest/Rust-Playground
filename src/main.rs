530213133cc59157ca7b421c2f18581e02cc4a11
Ernesto2018 <ernestondieki12@gmail.com>
2025-11-23 11:37:14 +0300

    Rust traits

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
-}
+use std::cmp::PartialOrd;
 
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
+/// get the largest item in a vec
+fn largest<T>(list: &[T]) -> &T
+where
+    T: PartialOrd,
+{
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
+
+    largest
 }
 
 fn main() {
-    let p1 = Point { x: 5, y: 10 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    let p3 = p1.mixup(p2);
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
 }
