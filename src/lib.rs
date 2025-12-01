a8a78bd8d3c15e1eb10b9fe7629fb3f92cdf3eca
Ernesto2018 <ernestondieki12@gmail.com>
2025-12-01 17:00:27 +0300

    Adding custom failure messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
 }
 
 #[cfg(test)]
@@ -7,8 +7,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn has_name() {
+        let rt = greeting("John");
+        assert!(
+            rt.contains("John"),
+            "Greeting did not contain name, value was `{rt}`"
+        );
     }
 }
